4b6d52a71ceed7275c3cdf1f6e0dc2ecb51caa5f
Cody Yantis <cyantis@gmail.com>
2019-04-28 21:55:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,4 +22,26 @@ class Dog
     DB[:conn].execute("DROP TABLE dogs")
   end
 
+  def save
+
+  end
+
+  def self.create(name:, breed:)
+  end
+
+  def self.find_by_id(id)
+  end
+
+  def self.find_or_create_by(name)
+  end
+
+  def self.new_from_db(row)
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def update
+  end
+
 end
