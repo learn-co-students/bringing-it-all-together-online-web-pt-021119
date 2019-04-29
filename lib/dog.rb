6bc5756d1f8dae2c66a5100d598bf2dc95804c7f
Cody Yantis <cyantis@gmail.com>
2019-04-29 21:43:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,28 +25,63 @@ class Dog
   end
 
   def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
 
+    if self.id
+      self.update
+    else
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
   end
 
   def self.create(hash)
-    dog = self.new(name: hash.name, breed: hash.breed)
+    dog = self.new(name: hash[:name], breed: hash[:breed])
     dog.save
     dog
   end
 
   def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+
+    result = DB[:conn].execute(sql, id).flatten
+    self.new(id: result[0], name: result[1], breed: result[2])
   end
 
   def self.find_or_create_by(name)
+    binding.pry
   end
 
   def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    result = DB[:conn].execute(sql, name).flatten
+    self.new(id: result[0], name: result[1], breed: result[2])
   end
 
   def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
